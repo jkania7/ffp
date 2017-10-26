954d1ae802db4f23eb62a4b0e101077f8f8210eb
jkania7 <jkania@alumni.cmu.edu>
2017-10-26 10:55:06 -0400

    remove dups

--- README.md
+++
@@ -1,4 +0,0 @@
-# ffp - Five Faint Pulsars - GBT17A-447
-Timing of five faint pulsars
-
-PI - Dunc Lorimer
--- cp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#created by jwk on 15sep17 to sync files from the obs. date to the psr name folders
-
-rsync -vrah /gbo/AGBT17A_477/*/*_1929+16_*.pfd* /gbo/AGBT17A_477/share/1929+16/
