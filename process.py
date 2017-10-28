6e1f2dc58291e118492793f27719612aba890e6a
jkania7 <jkania@alumni.cmu.edu>
2017-10-27 21:25:59 -0400

    updating processing script

--- process.py
+++ process.py
@@ -1,17 +1,33 @@
+"""Created by jwkania to process five faint pulsars.C26OCT2017:U26OCT2017"""
 import os,fnmatch
 #loopnumber = 0
-for dirname,dirnames, filenames  in os.walk('..'):
-    if '.git' in dirnames:
-        dirnames.remove('.git')
+verbose=True #prints out tests
+pulsars=["1929+16"] #list of pulsars you want to process.
 
-    containsFit = False; contiansPFD = False
-    for filename in filenames:
-        if fnmatch.fnmatch(filename,'1929+16*.fit'):
-            containsFIT = True
-        if fnmatch.fnmatch(filename,'_1929+16.pfd'):
-            containsPFD = True
-    if containsFit and not containsPFD:
-        print('I need a PFD file in {0}'.format(dirname))
+for pulsar in pulsars:
+    for dirname,dirnames, filenames  in os.walk('..'):
+        if '.git' in dirnames:#removes git dir
+            dirnames.remove('.git')
+        if verbose: print("dirname = {0}".format( dirname))
+        goAgain = True
+        while goAgain:
+            containsFit = False; contiansPFD = False; fitsNUM=[];fitsNAME=[];
+            for fitsNAME in filenames:
+                if verbose: print("fitsNAME= {0}".format(fitsNAME))
+                if fnmatch.fnmatch(fitsNAME,'*{0}*.fits'.format(pulsar)):
+                    containsFIT = True
+                    fitsNUM = (fitsNAME.split('.')[0]).split('_')[-1]#parses .fits file to get number
+                    if verbose: print("fitsNUM={0}".format(fitsNUM))
+
+            for filename in filenames:
+                if fnmatch.fnmatch(filename,'*{0}_PSR_{1}.pfd'.format(fitsNUM,pulsar)):
+                    containsPFD = True
+
+            if containsFit and not containsPFD:
+                print('You need a PFD {0}_PSR_{1}.pfd file in {1}'.format(fitsNum,pulsar,dirname))
+                os.system("predfold -timing ../{0}.par {1}/{2}".format(pulsar,dirname,fitsNAME))#tries to make the pfd file, par one up
+            else:
+                goAgain = False
     """
     for subdirnames in dirnames:
         print(os.path.join(dirname, subdirnames))
