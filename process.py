d58288258d83b0ae191e6b1a88b1aeaa2ea4600f
jkania7 <jkania@alumni.cmu.edu>
2017-11-02 09:48:33 -0400

    backup before moving

--- process.py
+++ process.py
@@ -1,7 +1,7 @@
 """Created by jwkania to process five faint pulsars.C26OCT2017:U26OCT2017"""
 import os,fnmatch
 #loopnumber = 0
-verbose = False #prints out tests
+verbose = True#prints out tests
 pulsars = ["1929+16","1929+19"] #list of pulsars you want to process.
 
 for pulsar in pulsars:
@@ -10,7 +10,7 @@ for pulsar in pulsars:
             dirnames.remove('.git')
         if verbose: print("dirname = {0}".format(dirname))
         goAgain = 0;
-        while (goAgain < 10):
+        while (goAgain < 5):
             containsFIT = False; fitsNUMs=[];fitsNAME=[];
             for fileNAME in filenames:
                 if verbose: print("fileNAME= {0}".format(fileNAME))
@@ -25,15 +25,17 @@ for pulsar in pulsars:
                 #print(len(fitsNUMs))
                 for i, fitsNUM in enumerate(fitsNUMs):
                     containsPFD = False
+                    print(pulsar)
                     if fnmatch.fnmatch(filename,'*{0}_PSR_{1}.pfd'.format(fitsNUM,pulsar)):
-                        #containsPFD = True
+                        print("filename={0}".format(filename))
+                        containsPFD = True
                         print("os.path.isfile(format(pulsar,filename))= {0}".format(os.path.isfile("./{0}/{1}".format(pulsar,filename))))
                         if not os.path.isfile("./{0}/{1}".format(pulsar,filename)):
                             print("You need {0} in {1}".format(filename,pulsar))
                             os.system("cp {0}/{1} ./{2}".format(dirname,filename, pulsar))
                     if (containsFIT) and (not containsPFD):
                         print('You need a PFD {0}_PSR_{1}.pfd file in {2}'.format(fitsNUM,pulsar,dirname))
-                        os.system("prepfold -timing ../{0}.par {1}/{2}".format(pulsar,dirname,fitsNAME[i]))#tries to make the pfd file, par one up
+                        os.system("prepfold -timing /gbo/AGBT17A_477/{0}.par /gbo/AGBT17A_477{1}/{2} -noxwin".format(pulsar,dirname[2:],fitsNAME[i]))#tries to make the pfd file, par one up
                     else:
                         goAgain = 10 #ends the loop after all fits files have been made
             print("goAgain = {0}".format(goAgain))
